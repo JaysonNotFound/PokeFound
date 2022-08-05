fc426f1619e9a7b8a64d26200c47ed5c36bca92d
Jayson Teano <jteano@sprout.ph>
2022-08-05 08:24:14 +0800

    remove core bloc

--- lib/modules/home/page/home_page.dart
+++ lib/modules/home/page/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 
-import '../../../core/blocs/main_blocs/main_blocs.dart';
+import '../../pokemon/module/pokemon/domain/bloc/pokemon_bloc.dart';
+import '../../pokemon/module/pokemon_details/domain/bloc/pokemon_details_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,7 +13,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: MainBlocs.get(),
+      providers: [
+        BlocProvider(create: (context) => GetIt.I<PokemonBloc>()),
+        BlocProvider(create: (context) => GetIt.I<PokemonDetailsBloc>()),
+      ],
       child: AutoRouter(),
     );
   }
